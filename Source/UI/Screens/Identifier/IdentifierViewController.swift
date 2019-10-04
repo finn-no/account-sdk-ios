b300deb2cbdc76e02911862b8bb60fd92dc0000b
Joakim Stien <joakim.stien@finn.no>
2019-10-04 09:47:56 +0200

    Revert "not automatically prefilling known emails"
    This reverts commit f33a5040a82484ab070de49aa83d423035555b73.

--- Source/UI/Screens/Identifier/IdentifierViewController.swift
+++ Source/UI/Screens/Identifier/IdentifierViewController.swift
@@ -156,6 +156,9 @@ class IdentifierViewController: IdentityUIViewController {
         case .email, .password:
             showEmailAddress()
             self.viewToEnsureVisibilityOfAfterKeyboardAppearance = self.emailAddress
+            if let savedEmail = Settings.value(forKey: Constants.EmailStorageLabel) {
+                self.emailAddress.text = savedEmail as? String
+            }
         case let .emailWithPrefilledValue(prefilledEmail), let .passwordWithPrefilledEmail(prefilledEmail):
             showEmailAddress()
             self.emailAddress.text = prefilledEmail.normalizedString
